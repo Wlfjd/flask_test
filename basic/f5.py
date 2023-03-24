599cb1549f916731eff1f3e414bf70d86c9f1fec
Wlfjd <leejiwon813@naver.com>
2023-03-24 15:49:28 +0900

    feat: 에러메세지를 범용적으로 처리하게끔 메세지를 전달도록 수정

--- basic/f5.py
+++ basic/f5.py
@@ -60,8 +60,9 @@ def login():
         # 3-2. 서비스 메일 화면으로 이동
         else: # 4. 회원아니면
         # 4-1. 적당한 메세지 후 다시 로그인 유도
-         return render_template('error.html')
-         
+        # render_template() => jinja2 템플릿 엔진 사용 ==> 문법 jinja2를 따라간다
+         return render_template('error.html', msg='로그인 실패')
+
         #return redirect('https://www.naver.com') # redirect : 요청을 다른 URL로 forwarding한다
 
 
