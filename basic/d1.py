b9096d957a3d16bd2b69112a99bc617c2aaefa96
Wlfjd <leejiwon813@naver.com>
2023-03-24 12:52:28 +0900

    feat: 고객 테이블 생성 및 구조(스키마)를 업데이트(수정)

--- basic/d1.py
+++ basic/d1.py
@@ -38,7 +38,31 @@
     4. 현재 작업(사용)할 데이터베이스 지정
         use ml_db;
         Database changed
-        MariaDB [ml_db]>
-'''
+        MariaDB [ml_db]> show tables;
+
+    5. 고객 테이블 생성
+    USING BTREE : 검색 속도를 높이는 방식 중에 하나로 특정 컬럼을 지정
+    CREATE TABLE `users` 
+        (
+        `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '고객 고유 관리 ID',
+        `uid` VARCHAR(32) NOT NULL COMMENT '고객 로그인 아이디' COLLATE 'utf8mb4_general_ci',
+        `upw` VARCHAR(128) NOT NULL COMMENT '고객 로그인 비번' COLLATE 'utf8mb4_general_ci',
+        `name` VARCHAR(30) NOT NULL COMMENT '고객 이름' COLLATE 'utf8mb4_general_ci',
+        `regdate` TIMESTAMP NOT NULL COMMENT '고객 가입일',
+        PRIMARY KEY (`id`) USING BTREE,
+        UNIQUE INDEX `uid` (`uid`) USING BTREE
+    )
+    COMMENT='고객 테이블'
+    COLLATE='latin1_swedish_ci'
+    ENGINE=InnoDB
+    ;
+
+    ALTER TABLE `users`
+    
+        CHANGE COLUMN `uid` `uid` VARCHAR(32) NOT NULL COLLATE 'latin1_swedish_ci' AFTER `id`,
+        CHANGE COLUMN `upw` `upw` VARCHAR(128) NOT NULL COLLATE 'latin1_swedish_ci' AFTER `uid`,
+        CHANGE COLUMN `name` `name` VARCHAR(30) NOT NULL COLLATE 'latin1_swedish_ci' AFTER `upw`,
+        CHANGE COLUMN `regdate` `regdate` TIMESTAMP NOT NULL AFTER `name`;
+    SELECT `DEFAULT_COLLATION_NAME` FROM `information_schema`.`SCHEMATA` WHERE `SCHEMA_NAME`='ml_db';
 
 import pymysql as my
